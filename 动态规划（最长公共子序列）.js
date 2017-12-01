0ec1c0fd9e336f3092f1a834b4823b823ed06693
Alliance <574014131@qq.com>
2017-12-01 13:46:25 +0800

    --

--- 动态规划（最长公共子序列）.js
+++ 动态规划（最长公共子序列）.js
@@ -1,6 +1,6 @@
 // 给定两个字符串str1和str2，返回两个字符串的最长公共子序列，例如：str1="1A2C3D4B56",str2="B1D23CA45B6A",
-// "123456"和"12C4B6"都是最长公共子序列，返回哪一个都行，dp[i][j]的值必然和dp[i-1][j],dp[i][j-1],dp[i-1][j-1]相关，
-// 从第1行和第1列开始计算的，把第0行和第0列都初始化为0，为了后面的取最大值在代码实现上的方便，dp[i][j]取三者之间的最大值
+// "123456"和"12C4B6"都是最长公共子序列，返回哪一个都行，
+// dp[i][j]的值必然和dp[i-1][j],dp[i][j-1],dp[i-1][j-1]相关，dp[i][j]取三者之间的最大值
 let str1="1A2C3D4B56";
 let str2="B1D23CA45B6A";
 function search(str1, str2) {
@@ -9,10 +9,9 @@ function search(str1, str2) {
     let arr=new Array(m).fill(0).map(v=>new Array(n).fill(0));
     for(let i=1;i<m;i++){
         for(let j=1;j<n;j++){
+            arr[i][j]=Math.max(arr[i-1][j],arr[i][j-1]);
             if(str2[i-1]===str1[j-1]){
-                arr[i][j]=1+arr[i-1][j-1];
-            }else{
-                arr[i][j]=Math.max(arr[i-1][j],arr[i][j-1]);
+                arr[i][j]=arr[i-1][j-1]+1;
             }
         }
     }
