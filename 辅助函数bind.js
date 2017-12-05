55de2e8a4f39d30f8380b37e7269e1db40af803f
Alliance <574014131@qq.com>
2017-12-05 20:56:04 +0800

    --

--- 辅助函数bind.js
+++ 辅助函数bind.js
@@ -30,3 +30,16 @@ if(!Function.prototype.bind){
         return fBound;
     }
 }
+// 软绑定
+if(!Function.prototype.softBind){
+    Function.prototype.softBind=function (obj) {
+        var fn=this;
+        // 捕获所有curried参数
+        var curried=[].slice.call(arguments,1);
+        var bound=function () {
+            return fn.apply( (!this || this === (window||global)) ? obj : this,curried.concat.apply(curried,arguments) );
+        }
+        bound.prototype=Object.create(fn.prototype);
+        return bound;
+    }
+}
