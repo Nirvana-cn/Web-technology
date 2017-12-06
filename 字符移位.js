559f29f29749bc66327f7440834dd99b31bbee65
Alliance <574014131@qq.com>
2017-12-06 13:24:26 +0800

    --

--- 字符移位.js
+++ 字符移位.js
@@ -1,3 +1,4 @@
+// 把一个字符串的大写字母放到字符串的后面，各个字符的相对位置不变，且不能申请额外的空间
 function changeStr(str) {
     let arr=Array.from(str);
     let temp=[];
@@ -9,3 +10,19 @@ function changeStr(str) {
     return arr.concat(temp).join('');
 }
 changeStr('AfderFVfdEOG');
+
+// var readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+// rl.on('line', function(line){
+//     let arr=Array.from(line);
+//     let temp=[];
+//     for(let i=arr.length-1;i>=0;i--){
+//         if(arr[i].charCodeAt(0)<=90){
+//             temp.unshift(arr.splice(i,1)[0])
+//         }
+//     }
+//     console.log(arr.concat(temp).join(''));
+// });
