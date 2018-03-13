26f042322a15a6d2a67166e1d2fb675b12041654
Alliance <574014131@qq.com>
2018-03-13 21:48:05 +0800

    --

--- JS/手写ajax.js
+++ JS/手写ajax.js
@@ -13,6 +13,6 @@ xhr.open('method','url');
 xhr.send('data');
 xhr.onreadystatechange=function () {
     if(xhr.readyState==4 && xhr.status==200){
-        let data=xhr.xhr.responseText;
+        let data=xhr.responseText;
     }
 }
