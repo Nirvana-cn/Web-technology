e2624fbc1e40fb29f5db04e55834318d6867b1ef
Alliance <574014131@qq.com>
2018-04-17 20:44:51 +0800

    learn mvvm

---
+++ JS/mvvm.js
@@ -0,0 +1,142 @@
+function Mvvm(options = {}) {
+    // vm.$options Vue上是将所有属性挂载到上面
+    // 所以我们也同样实现,将所有属性挂载到了$options
+    this.$options = options;
+    // this._data 这里也和Vue一样
+    let data = this._data = this.$options.data;
+
+    // 数据劫持
+    observe(data);
+    for (let key in data) {
+        Object.defineProperty(this, key, {
+            configurable: true,
+            get() {
+                return this._data[key];     // 如this.a = {b: 1}
+            },
+            set(newVal) {
+                this._data[key] = newVal;
+            }
+        });
+    }
+
+    new Compile(options.el, this);
+
+}
+
+// 创建一个Observe构造函数
+// 写数据劫持的主要逻辑
+function Observe(data) {
+    let dep = new Dep();
+    // 所谓数据劫持就是给对象增加get,set
+    // 先遍历一遍对象再说
+    for (let key in data) {     // 把data属性通过defineProperty的方式定义属性
+        let val = data[key];
+        observe(val);   // 递归继续向下找，实现深度的数据劫持
+        Object.defineProperty(data, key, {
+            configurable: true,
+            get() {
+                Dep.target && dep.addSub(Dep.target);
+                return val;
+            },
+            set(newVal) {   // 更改值的时候
+                if (val === newVal) {   // 设置的值和以前值一样就不理它
+                    return;
+                }
+                val = newVal;   // 如果以后再获取值(get)的时候，将刚才设置的值再返回去
+                observe(newVal);    // 当设置为新值后，也需要把新值再去定义成属性
+                dep.notify();
+            }
+        });
+    }
+}
+
+// 外面再写一个函数
+// 不用每次调用都写个new
+// 也方便递归调用
+function observe(data) {
+    // 如果不是对象的话就直接return掉
+    // 防止递归溢出
+    if (!data || typeof data !== 'object') return;
+    return new Observe(data);
+}
+
+function Compile(el, vm) {
+    // 将el挂载到实例上方便调用
+    vm.$el = document.querySelector(el);    //document.querySelector获取一个元素，document.querySelectorAll获取全部符合的元素
+    // 在el范围里将内容都拿到，当然不能一个一个的拿
+    // 可以选择移到内存中去然后放入文档碎片中，节省开销
+    let fragment = document.createDocumentFragment();
+
+    while (child = vm.$el.firstChild) {
+        fragment.appendChild(child);    // 因为第一个子元素可能为换行，所以要进行判断，当为真时，将el中的内容放入内存中
+    }
+    // 对el里面的内容进行替换
+    function replace(frag) {
+        Array.from(frag.childNodes).forEach(node => {
+            let txt = node.textContent;
+            let reg = /\{\{(.*?)\}\}/g;   // 正则匹配{{}}
+
+            if (node.nodeType === 3 && reg.test(txt)) { // 即是文本节点又有大括号的情况{{}}
+                console.log(RegExp.$1); // 匹配到的第一个分组 如： a.b, c
+                let arr = RegExp.$1.split('.');
+                let val = vm;
+                arr.forEach(key => {
+                    val = val[key];     // 如this.a.b
+                });
+                // 用trim方法去除一下首尾空格
+                node.textContent = txt.replace(reg, val).trim();
+                new Watcher(vm, RegExp.$1, newVal => {
+                    node.textContent = txt.replace(reg, newVal).trim();
+                });
+            }
+            // 如果还有子节点，继续递归replace
+            if (node.childNodes && node.childNodes.length) {
+                replace(node);
+            }
+        });
+    }
+
+    replace(fragment);  // 替换内容
+
+    vm.$el.appendChild(fragment);   // 再将文档碎片放入el中
+}
+
+// 发布订阅模式  订阅和发布 如[fn1, fn2, fn3]
+function Dep() {
+    // 一个数组(存放函数的事件池)
+    this.subs = [];
+}
+Dep.prototype = {
+    addSub(sub) {
+        this.subs.push(sub);
+    },
+    notify() {
+        // 绑定的方法，都有一个update方法
+        this.subs.forEach(sub => sub.update());
+    }
+};
+
+// 重写Watcher构造函数
+function Watcher(vm, exp, fn) {
+    this.fn = fn;
+    this.vm = vm;
+    this.exp = exp;
+    // 添加一个事件
+    // 这里我们先定义一个属性
+    Dep.target = this;
+    let arr = exp.split('.');
+    let val = vm;
+    arr.forEach(key => {    // 取值
+        val = val[key];     // 获取到this.a.b，默认就会调用get方法
+    });
+    Dep.target = null;
+}
+
+Watcher.prototype.update = function() {
+    let arr = this.exp.split('.');
+    let val = this.vm;
+    arr.forEach(key => {
+        val = val[key];   // 通过get获取到新的值
+    });
+    this.fn(val);
+};
