b62ec144a5ca7a9b1be3031f00e0d5c812540402
Alliance <574014131@qq.com>
2018-05-08 15:35:49 +0800

    packing ajax with promise

---
+++ JS/Promise封装ajax.js
@@ -0,0 +1,26 @@
+function ajax(url, method) {
+    function getXhr() {
+        let xhr=null
+        if(window.XMLHttpRequest){
+            xhr=new XMLHttpRequest()
+        }else{
+            xhr=new ActiveXObject("Microsoft.XMLHttp")
+        }
+        return xhr;
+    }
+    return new Promise(function (resolve, reject) {
+        let xhr=getXhr();
+        xhr.open(method,url);
+        xhr.send('data');
+        xhr.onreadystatechange=function () {
+            if(xhr.readyState===4 && xhr.status===200){
+                let data=xhr.responseText;
+                resolve(data)
+            }else{
+                let resJson = { code: this.status, response: this.response }
+                reject(resJson)
+            }
+        }
+    })
+
+}
