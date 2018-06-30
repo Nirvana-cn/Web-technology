9854646df4d9dc947f3575bea1540d404fd6a1cf
Alliance <574014131@qq.com>
2018-06-30 15:36:35 +0800

    throttle & debounce

---
+++ JS/去抖函数.js
@@ -0,0 +1,10 @@
+function debounce(method,delay){
+    var timer=null;
+    return function(){
+        var context=this, args=arguments;
+        clearTimeout(timer);
+        timer=setTimeout(function(){
+            method.apply(context,args);
+        },delay);
+    }
+}
---
+++ JS/节流函数.js
@@ -0,0 +1,10 @@
+function throttle(method,duration){
+    var  begin=new Date();
+    return function(){
+        var context=this, args=arguments, current=new Date();
+        if(current-begin>=duration){
+            method.apply(context,args);
+            begin=current;
+        }
+    }
+}
