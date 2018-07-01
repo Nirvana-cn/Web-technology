838ca3d8ea2601821cc06fba6e785b6f5bab617d
Alliance <574014131@qq.com>
2018-07-01 20:14:36 +0800

    throttle & debounce

--- JS/去抖函数.js
+++ JS/去抖函数.js
@@ -1,3 +1,6 @@
+// 函数防抖是函数在特定的时间内不被再调用后执行，比如：
+// 1. scroll/resize事件
+// 2. 文本连续输入，ajax验证/关键字搜索
 function debounce(method,delay){
     var timer=null;
     return function(){
--- JS/节流函数.js
+++ JS/节流函数.js
@@ -1,3 +1,9 @@
+// 函数节流是确保函数特定的时间内至多执行一次，比如：
+// 1. 频繁的mousemove/keydown，比如高频的鼠标移动，游戏射击类的
+// 2. 搜索联想（keyup）
+// 3. 进度条（我们可能不需要高频的更新进度）
+// 4. 拖拽的dragover等
+// 5.  高频的点击，抽奖等
 function throttle(method,duration){
     var  begin=new Date();
     return function(){
