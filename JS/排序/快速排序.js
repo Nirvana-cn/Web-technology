b4d72597d1d9197da6a0254f5d059a190c7d23e4
Alliance <574014131@qq.com>
2018-07-25 22:17:00 +0800

    --

--- JS/排序/快速排序.js
+++ JS/排序/快速排序.js
@@ -42,3 +42,15 @@ function quickSort(myArray, left, right) {
 function quickSort(a) {
     return a.length <= 1 ? a : quickSort(a.slice(1).filter(item => item <= a[0])).concat(a[0], quickSort(a.slice(1).filter(item => item > a[0])));
 }
+
+// 使用filter
+function quickSort(arr) {
+    // 当数组长度不大于1时，返回结果，防止callstack溢出。
+    if(arr.length <= 1) return arr;
+    return [
+        // 递归调用quickSort，通过Array.prototype.filter方法过滤小于arr[0]的值，注意去掉了arr[0]以防止出现死循环。
+        ...quickSort(arr.slice(1).filter(item => item < arr[0])),
+        arr[0],
+        ...quickSort(arr.slice(1).filter(item => item >= arr[0]))
+    ];
+}
