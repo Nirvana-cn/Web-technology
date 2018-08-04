4e8f09b8a3d4c00878d6a699833149db8d361d00
Alliance <574014131@qq.com>
2018-08-04 10:33:36 +0800

    --

--- JS/深拷贝.js
+++ JS/深拷贝.js
@@ -1,13 +1,69 @@
-function deepCopy(source, target = {}) {
-    for (key in source) {
-        if (source.hasOwnProperty(key)) {                         // 意思就是__proto__上面的属性,我不拷贝
-            if (typeof(source[key]) === "object") {               // 如果这一项是object类型,就递归调用deepCopy
-                target[key] = Array.isArray(source[key]) ? [] : {};
-                deepCopy(source[key], target[key]);
-            } else {                                            // 如果不是object类型,就直接赋值拷贝
-                target[key] = source[key];
-            }
+const clone = parent => {
+    // 维护两个储存循环引用的数组
+    const parents = [];
+    const children = [];
+
+    const _clone = parent => {
+        if (parent === null) return null;
+        if (typeof parent !== 'object') return parent;
+
+        let child, proto;
+
+        if (class2type[toString.call(parent)]==='array') {
+            // 对数组做特殊处理
+            child = [];
+        } else if (class2type[toString.call(parent)]==='regexp') {
+            // 对正则对象做特殊处理
+            child = new RegExp(parent.source, getRegExp(parent));
+            if (parent.lastIndex) child.lastIndex = parent.lastIndex;
+        } else if (class2type[toString.call(parent)]==='date') {
+            // 对Date对象做特殊处理
+            child = new Date(parent.getTime());
+        } else {
+            // 处理对象原型
+            proto = Object.getPrototypeOf(parent);
+            // 利用Object.create切断原型链
+            child = Object.create(proto);
         }
-    }
-    return target;
+
+        // 处理循环引用
+        const index = parents.indexOf(parent);
+
+        if (index != -1) {
+            // 如果父数组存在本对象,说明之前已经被引用过,直接返回此对象
+            return children[index];
+        }
+        parents.push(parent);
+        children.push(child);
+
+        for (let i in parent) {
+            // 递归
+            child[i] = _clone(parent[i]);
+        }
+
+        return child;
+    };
+    return _clone(parent);
+};
+
+var class2type = {
+    "[object Boolean]": "boolean",
+    "[object Number]": "number",
+    "[object String]": "string",
+    "[object Function]": "function",
+    "[object Array]": "array",
+    "[object Date]": "date",
+    "[object RegExp]": "regexp",
+    "[object Object]": "object",
+    "[object Error]": "error",
+    "[object Symbol]": "symbol"
 }
+var toString = Object.prototype.toString;
+
+const getRegExp = re => {
+    var flags = '';
+    if (re.global) flags += 'g';
+    if (re.ignoreCase) flags += 'i';
+    if (re.multiline) flags += 'm';
+    return flags;
+};
