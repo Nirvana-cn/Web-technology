0cd8f9e6913cc519db1769dd8dc42c507779b65b
Alliance <574014131@qq.com>
2019-01-26 12:09:23 +0800

    emoij

---
+++ JS/父子关系的数组转Tree结构.js
@@ -0,0 +1,28 @@
+let list = [
+    {
+        id: 'a',
+        pid: 'b',
+        otherData: {}
+    }, {
+        id: 'b',
+        pid: null,
+        otherData: {}
+    }, {
+        id: 'c',
+        pid: 'a',
+        otherData: {}
+    }, {
+        id: 'd',
+        pid: 'a',
+        otherData: {}
+    }, {
+        id: 'e',
+        pid: 'b',
+        otherData: {}
+    }, {
+        id: 'f',
+        pid: null,
+        otherData: {}
+    }
+]
+
